869a0cae7469eee3adb8a2232c385912271e6e97
Jayeen28 <jayeen6783@gmail.com>
2022-04-17 13:45:19 +0600

    stable again

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 const managePrice = async (page, browser) => {
-    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-    const price = await page.evaluate(() => document.getElementById('corePrice_desktop').childNodes[1].innerText.split('\n')[2].replace(/[^a-zA-Z0-9.]/g, ''));
-    if (price <= 160) {
-        browser.close();
-        process.exit(0);
-    } else {
-        console.log('still high');
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded", "networkidle2"] });
+    const price = await page.evaluate(() => document.querySelectorAll("[data-a-color=price]")[0]?.innerText?.split('\n')[0]?.replace(/[^a-zA-Z0-9.]/g, ''));
+    if (price) {
+        if (price <= 160) {
+            browser.close();
+            process.exit(0);
+        } else {
+            console.log('still high');
+        }
+    }
+    else {
+        console.log('Price not found')
     }
 };
 
